dcafd46235c0447c95314ccbd4f84cbe6b3191ca
abrar-yo <37622264+abrar-yo@users.noreply.github.com>
2026-02-05 00:05:19 +0300

    update nominee info

--- models/insurance_nominee.py
+++ models/insurance_nominee.py
@@ -21,13 +21,42 @@ class InsuranceNominee(models.Model):
     _rec_name = 'partner_id'
 
     partner_id = fields.Many2one("res.partner", string="Name", domain="[('is_agent', '=', False)]")
-    insurance_nominee_relation_id = fields.Many2one('insurance.nominee.relation',
-                                                    string="Relation with Policy Holder")
+    # insurance_nominee_relation_id = fields.Many2one('insurance.nominee.relation',
+    #                                                 string="Relation with Policy Holder")
     nominee_dob = fields.Date(string="Date of Birth")
     nominee_age = fields.Char(string="Age", compute="_compute_nominee_age_count", translate=True)
-    percentage = fields.Float(string="Percentage")
+    insured_gender = fields.Selection([
+        ('male', "Male"),
+        ('female', "Female"),
+        ('others', "Others")],
+        string=" Gender")
+    insured_blood_group = fields.Selection(
+        [('a_positive', "A+"),
+         ('a_negative', "A-"),
+         ('b_positive', "B+"),
+         ('b_negative', "B-"),
+         ('ab_positive', "AB+"),
+         ('ab_negative', "AB-"),
+         ('o_positive', "O+"),
+         ('o_negative', "O-")],
+        string="Blood Group")
+    insured_heights = fields.Char(string="Height(cm)")
+    insured_weights = fields.Char(string="Weight(kg)")
+    insured_birthmarks = fields.Char(string="Birthmark")
+    insured_marital_status = fields.Selection([
+        ('single', "Single"),
+        ('married', "Married"),
+        ('divorced', "Divorced"),
+        ('widowed', "Widowed"),
+        ('separated', "Separated"),
+        ('annulled', "Annulled"),
+        ('domestic', "Domestic Partnership/Civil Union"),
+        ('common', "Common-Law Marriage"),
+        ('other', "Other")], string="Marital Status")
     insurance_information_id = fields.Many2one('insurance.information', ondelete='cascade')
     claim_information_id = fields.Many2one('claim.information', ondelete='cascade')
+    company_id = fields.Many2one('res.company', string='Policy Holder')
+    medical_history_ids = fields.One2many('medical.history', 'insure_nominee_id',string="Medical History")
 
     @api.constrains('nominee_dob')
     def _check_nominee_dob(self):
@@ -51,3 +80,13 @@ class InsuranceNominee(models.Model):
                 rec.nominee_age = f"{max(nominee_age, 0)} Years"
             else:
                 rec.nominee_age = "0 Years"
+
+class MedicalHistory(models.Model):
+    _name = 'medical.history'
+    _description = __doc__
+    _rec_name = 'name'
+
+    name = fields.Char(translate=True, string="Disease")
+    disease_year = fields.Char(string="Year of Disease")
+    notes = fields.Text(string="Medical Notes")
+    insure_nominee_id = fields.Many2one('insurance.nominee')
